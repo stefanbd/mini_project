3a9a713b3926f0baab4aab1bbf0331894ec3b19c
stefan <generation@dichev.me>
2021-08-02 11:32:41 +0100

    comment

---
+++ data/init.sql
@@ -0,0 +1,26 @@
+CREATE DATABASE IF NOT EXISTS store;
+
+CREATE TABLE IF NOT EXISTS store.products (
+product_id int NOT NULL AUTO_INCREMENT,
+product_name varchar(22) NOT NULL,
+product_price float NOT NULL,
+PRIMARY KEY (product_id)
+);
+
+CREATE TABLE IF NOT EXISTS store.couriers (
+courier_id int NOT NULL AUTO_INCREMENT,
+courier_name varchar(22) NOT NULL,
+courier_phone float NOT NULL,
+PRIMARY KEY (courier_id)
+);
+
+CREATE TABLE IF NOT EXISTS store.orders (
+order_id int NOT NULL AUTO_INCREMENT,
+customer_name varchar(22) NOT NULL,
+customer_surname varchar(22) NOT NULL,
+customer_address varchar(100) NOT NULL,
+courier_id int NOT NULL,
+order_status varchar(22)
+PRIMARY KEY (order_id),
+FOREIGN KEY (courier_id) REFERENCES couriers (courier_id)
+);
